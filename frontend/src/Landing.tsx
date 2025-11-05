17393ce0942419723212173f6660b6532b283c95
Kritagya123611 <jhakritagya45@gmail.com>
2025-11-05 17:43:58 +0530

    Add global .gitignore

---
+++ frontend/src/Landing.tsx
@@ -0,0 +1,7 @@
+export default function Landing() {
+    return(
+        <div className="min-h-screen bg-black flex flex-col justify-center items-center gap-6">
+          <h1 className="text-white text-4xl">Welcome to the Landing Page</h1>
+        </div>
+    )
+}
